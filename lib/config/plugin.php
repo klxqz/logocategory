2ce904bcfee5cfb62537343a6dda9b404f74a478
klxqz <klxqz@mail.ru>
2013-11-22 13:39:28 +0600

    смена разработчика

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,13 +3,13 @@
 return array(
     'name' => 'Логотип категории',
     'description' => 'Плагин позволяет добавить логотип категории',
-    'vendor'=>903438,
-    'version'=>'1.0.0',
-    'img'=>'img/logocategory.png',
+    'vendor' => '985310',
+    'version' => '1.0.0',
+    'img' => 'img/logocategory.png',
     'shop_settings' => true,
-    'frontend'    => true,
-    'icons'=>array(
-        16=>'img/logocategory.png',
+    'frontend' => true,
+    'icons' => array(
+        16 => 'img/logocategory.png',
     ),
     'handlers' => array(
         'backend_category_dialog' => 'backendCategoryDialog',
@@ -17,6 +17,5 @@ return array(
         'frontend_category' => 'frontendCategory',
         'category_delete' => 'categoryDelete',
     ),
-
 );
 //EOF
